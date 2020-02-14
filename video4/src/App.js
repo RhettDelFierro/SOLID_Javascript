9fd068429eb7dc993753a9e242bd5b5d66bef958
Rhett Del Fierro <thinquetank@gmail.com>
2020-02-14 09:02:48 -0800

    added state.

--- video4/src/App.js
+++ video4/src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props) //to make sure we don't break anything in the base class
+    this.state = {
+      username: '',
+      quantity: 0,
+      colors: [],
+      'mailing-list': false
+    }
+  }
   stateJson = () => {
     return JSON.stringify(this.state)
   }
@@ -29,7 +38,18 @@ class App extends Component {
             <option value='indigo'>Indigo</option>
             <option value='violet'>Violet</option>
           </select>
+        </div>
+        <div className='App-field'>
+          <label className='App-label' htmlFor='field-mailing-list'>Join Our Mailing List</label>
+          <input type='checkbox' name='mailing-list' id='field-mailing-list'/>
+        </div>
 
+        <div className='App-field'>
+          <button className='App-button'>Submit</button>
+        </div>
+
+        <div className='App-field'>
+          <textarea className='App-json' value={this.stateJson()} readOnly/>
         </div>
       </div>
     )
