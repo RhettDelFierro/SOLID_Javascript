cd01f26f64327d202c01ca823b969399dc0b8f98
Rhett Del Fierro <thinquetank@gmail.com>
2020-02-14 08:27:56 -0800

    boilerplate for video 4

--- video4/src/App.js
+++ video4/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  stateJson = () => {
+    return JSON.stringify(this.state)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='App-field'>
+          <label className='App-label' htmlFor='field-username'>Your Username</label>
+          <input type='text' name='username' id='field-username'/>
+        </div>
+        <div className='App-field'>
+          <label className='App-label' htmlFor='field-quantity'>Your Username</label>
+          <input type='number' name='quantity' id='field-quantity'/>
+        </div>
+        <div className='App-field'>
+          <label className='App-label' htmlFor='field-colors'>Your Username</label>
+          <select className='App-multi-select' name='colors' id='field-colors'>
+            <option value='red'>Red</option>
+            <option value='orange'>Orange</option>
+            <option value='yellow'>Yellow</option>
+            <option value='green'>Green</option>
+            <option value='blue'>Blue</option>
+            <option value='indigo'>Indigo</option>
+            <option value='violet'>Violet</option>
+          </select>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
