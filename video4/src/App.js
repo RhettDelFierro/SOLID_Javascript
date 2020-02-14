2c98bcd6d9c7434c98b1838241f8ee9429e3d7e8
Rhett Del Fierro <thinquetank@gmail.com>
2020-02-14 09:22:05 -0800

    added NumericInput subclass that extends Input.

--- video4/src/App.js
+++ video4/src/App.js
@@ -2,17 +2,33 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// subclassing the input:
+class NumericInput extends Input { //---------EXTENDS >INPUT<--------------
+  field = () => {
+    return (
+      // HARDCODED to be type 'number'
+      <input type='number' name={this.props.name} id={this.fieldId()} onChange={this.props.onChange}/>
+    )
+  }
+}
+
 // Adding form fields as sub components to move responsibility out of the app component and into subcomponents:
 class Input extends Component {
   fieldId = () => {
     return `field-${this.props.name}`
   }
 
+  // more reuse by making the types of input configurable. The output will be overwritten by a subclass.
+  field = () => {
+    return (
+      <input type='text' name={this.props.name} id={this.fieldId()} onChange={this.props.onChange}/>
+    )
+  }
   render() {
     return (
       <div className='App-field'>
         <label className='App-label' htmlFor={this.fieldId()}>{this.props.label}</label> /*this.props.label to generalize and re-use this component*/
-        <input type='text' name={this.props.name} id='field-username' onChange={this.props.onChange}/>
+        {this.field()}
       </div>
     )
   }
@@ -49,10 +65,8 @@ class App extends Component {
     return (
       <div className="App">
         <Input name='username' label='Your Username' onChange={this.updateField}/>
-        <div className='App-field'>
-          <label className='App-label' htmlFor='field-quantity'>Your Username</label>
-          <input type='number' name='quantity' id='field-quantity'/>
-        </div>
+        <NumericInput name='quantity' label='Quantity' onChange={this.updateField}/>
+
         <div className='App-field'>
           <label className='App-label' htmlFor='field-colors'>Your Username</label>
           <select className='App-multi-select' name='colors' id='field-colors'>
