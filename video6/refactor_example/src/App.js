09e16407de2432cf244e0ccfd278b5b2d2d599c4
Rhett Del Fierro <thinquetank@gmail.com>
2020-02-15 12:07:34 -0800

    added some notes about policies and details that are still mixing.

--- video6/refactor_example/src/App.js
+++ video6/refactor_example/src/App.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ *  In this commit we were able to use a consistent interface [fetchColorData() and updateColorCount(id, count)]
+ * in order to build both a PaintJsClient and SuperInvestorClient.
+ * and now we can switch between them in order to access any of these servers.
+ * However, there is a question: where should that decision be made?
+ * Where is the right inflection point to decide whether we're using the old legacy client or the new investor client?
+
+ * The Dependency Inversion Principle supports the idea of being able to swap out.
+ *
+ * As of right now, we're still mixing the high-level and low-level details together:
+ * The high-level policies: 
+ * -we need to be able to fetchColorData and updateColorCount
+ * The low-level implementation details:
+ * -that we want to use either PainJsClient or SuperInvestorClient
+ *
+ * ^^ are still intermingled so we have to figure out where is the right place in the code to make those kind of decisions.
+*/
 
 class PaintJsClient {
   fetchColorData = () => {
@@ -52,8 +69,13 @@ class SuperInvestorClient {
     })
   }
 
+  // originally this was updateColorCount(id, count) but the new server knows nothing about ids, it only knows hex codes.
+  // We DON'T want to just change the function signature of this method, because that'd break the interfaces already being used.
+  // ^^ that would be a LSP violation to have one of these new objects change the rule/interface by HOW it's being used.
+  // Instead we'll translate the id to a hex in the function.
   updateColorCount = (id, count) => {
-    return fetch(`http://bigbucksinvestor.com/hexes/${id}`, {
+    const hexValue = this.idToHex(id)
+    return fetch(`http://bigbucksinvestor.com/hexes/${hexValue}`, {
       method: 'POST',
       mode: 'cors',
       header: {
@@ -83,6 +105,17 @@ class SuperInvestorClient {
     }
 
     return map[hex]
+  }
+
+  idToHex = (id) => {
+    const map = {
+      1: 'cc5c',
+      2: '768fb8',
+      3: '9eae65',
+      4: 'ffd700'
+    }
+
+    return map[id]
   }
 }
 
